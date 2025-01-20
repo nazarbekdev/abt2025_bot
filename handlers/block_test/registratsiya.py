2ba5bb7024f15c62c3fc930cbc709d7abf0a1114
nazarbekdev <nqobulov571@gmail.com>
2025-01-20 19:37:41 +0500

    registratsiya yakunlandi

--- handlers/block_test/registratsiya.py
+++ handlers/block_test/registratsiya.py
@@ -2,11 +2,15 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton, ContentType
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from datetime import timedelta
-from datetime import datetime
+from datetime import timedelta, datetime
+from dotenv import load_dotenv
 from loader import dp
 from keyboards.button.main_kyb import main_keyboard
 import os
+import re
+import requests
+
+load_dotenv()
 
 
 # Foydalanuvchi holatini kuzatish uchun State sinfi
@@ -28,7 +32,24 @@ def get_next_sunday():
     if days_to_sunday == 0:
         days_to_sunday = 7
     next_sunday = today + timedelta(days=days_to_sunday)
-    return next_sunday
+    sunday = str(next_sunday)
+    day, month, year = sunday.split('-')[2], sunday.split('-')[1], sunday.split('-')[0][2:]
+    date = f"{day}.{month}.{year}"
+    return date
+
+
+def format_phone_number(phone_number):
+    # Faqat raqamlarni olish uchun belgilarni tozalash
+    digits = re.sub(r'\D', '', phone_number)
+
+    # Raqam uzunligi tekshiriladi
+    if len(digits) == 12 and digits.startswith("998"):
+        # Namunadagi formatga keltirish
+        formatted = f"+998 {digits[3:5]} {digits[5:8]} {digits[8:10]} {digits[10:]}"
+    else:
+        raise ValueError("Noto'g'ri telefon raqami formati")
+
+    return formatted
 
 
 # Inline viloyatlar va fanlar
@@ -41,8 +62,22 @@ vaqt_intervallari = ['08:00 - 11:00', '10:00 - 13:00', '14:00 - 17:00', '18:00 -
 
 @dp.message_handler(lambda message: message.text == "📋 Ro'yxatdan o'tish")
 async def register_start(message: types.Message):
-    await RegistrationStates.ism_familiya.set()
-    await message.answer("Ism va familiyangizni kiriting", reply_markup=types.ReplyKeyboardRemove())
+    user_id = message.from_user.id
+    url_get = os.getenv("BLOK_TEST_GET")
+    req_get = requests.get(f"{url_get}{user_id}")
+    if req_get.status_code == 200:
+        status_date = True
+        for date in req_get.json():
+            if get_next_sunday() == date['rejalashtirilgan_vaqt'].split("⏰")[0][1:].strip():
+                status_date = False
+                user_date = date['rejalashtirilgan_vaqt']
+                await message.answer(f"Siz  {user_date}  blok test uchun ro'yxatdan o'tgansiz 😊")
+        if status_date:
+            await RegistrationStates.ism_familiya.set()
+            await message.answer("Ism va familiyangizni kiriting", reply_markup=types.ReplyKeyboardRemove())
+    else:
+        await RegistrationStates.ism_familiya.set()
+        await message.answer("Ism va familiyangizni kiriting", reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.message_handler(state=RegistrationStates.ism_familiya)
@@ -64,10 +99,10 @@ async def get_name(message: types.Message, state: FSMContext):
 
 @dp.message_handler(content_types=[ContentType.TEXT, ContentType.CONTACT], state=RegistrationStates.telefon_raqam)
 async def get_phone(message: types.Message, state: FSMContext):
-    if message.content_type == ContentType.CONTACT:  # Tugma orqali yuborilgan
+    if message.content_type == ContentType.CONTACT:
         contact = message.contact
         phone_number = contact.phone_number
-    elif message.content_type == ContentType.TEXT:  # Qo'lda kiritilgan
+    elif message.content_type == ContentType.TEXT:
         phone_number = message.text
         # Telefon raqamini tekshirish
         if not phone_number.startswith('+') and not phone_number.startswith('998'):
@@ -77,10 +112,8 @@ async def get_phone(message: types.Message, state: FSMContext):
         await message.answer("Telefon raqamingizni noto'g'ri yubordingiz. Iltimos, qayta urinib ko'ring.")
         return
 
-    # Telefon raqamni saqlash
     await state.update_data(telefon_raqam=phone_number)
 
-    # Reply keyboardni o'chirish
     tel_raqam = await message.answer("Telefon raqamingiz qabul qilindi!", reply_markup=types.ReplyKeyboardRemove())
 
     # Keyingi holat: Viloyatni tanlash
@@ -147,7 +180,6 @@ async def get_schedule(call: types.CallbackQuery, state: FSMContext):
     await call.message.edit_reply_markup(reply_markup=None)  # Tugmalarni olib tashlash
     await call.message.delete()  # Eski xabarni o‘chirish
 
-
     user_data = await state.get_data()
     summary = f"""📋 Sizning ma’lumotlaringiz\n
 👤 Ism: {user_data['ism_familiya']}
@@ -168,16 +200,33 @@ async def get_schedule(call: types.CallbackQuery, state: FSMContext):
 @dp.callback_query_handler(lambda c: c.data == 'confirm_', state=RegistrationStates.confirm)
 async def confirm_registration(call: types.CallbackQuery, state: FSMContext):
     user_data = await state.get_data()
-    await call.message.edit_reply_markup(reply_markup=None)  # Tugmalarni olib tashlash
-    await call.message.delete()  # Eski xabarni o‘chirish
-    await call.message.answer("✅ Siz muvaffaqiyatli ro‘yxatdan o‘tdingiz!", reply_markup=main_keyboard())
-    await call.message.answer(f"Sizga {get_next_sunday()} kuni soat {user_data['rejalashtirilgan_vaqt'][:5]} da test materiallari yuboriladi, belgilangan vaqt ichida javoblarni yuborishingizni so'rab qolamiz!\nIlmiz ziyoda bo'lsin!")
+    user_id = call.from_user.id
+    phone_number = format_phone_number(user_data['telefon_raqam'])
+    url_post = os.getenv('BLOK_TEST_POST')
+    req = requests.post(url_post, data={"telegram_id": user_id,
+                                        "ism_familiya": user_data['ism_familiya'],
+                                        "telefon_raqam": phone_number,
+                                        "viloyat": user_data['viloyat'],
+                                        "fan1": user_data['fan1'],
+                                        "fan2": user_data['fan2'],
+                                        "rejalashtirilgan_vaqt": f"📅 {get_next_sunday()} ⏰ {user_data['rejalashtirilgan_vaqt']}",
+                                        "status": "kutmoqda"})
+    if req.status_code == 200:
+        await call.message.edit_reply_markup(reply_markup=None)
+        await call.message.delete()
+        await call.message.answer("✅ Siz muvaffaqiyatli ro‘yxatdan o‘tdingiz!", reply_markup=main_keyboard())
+        await call.message.answer(
+            f"Sizga {get_next_sunday()} kuni soat {user_data['rejalashtirilgan_vaqt'][:5]} da test materiallari yuboriladi, belgilangan vaqt ichida javoblarni yuborishingizni so'rab qolamiz!\nIlmiz ziyoda bo'lsin!")
+    else:
+        await call.message.edit_reply_markup(reply_markup=None)
+        await call.message.delete()
+        await call.message.answer("Ro'yxatdan o'tishda xatolik")
     await state.finish()
 
 
 @dp.callback_query_handler(lambda c: c.data == 'cancel_', state=RegistrationStates.confirm)
 async def cancel_registration(call: types.CallbackQuery, state: FSMContext):
-    await call.message.edit_reply_markup(reply_markup=None)  # Tugmalarni olib tashlash
-    await call.message.delete()  # Eski xabarni o‘chirish
+    await call.message.edit_reply_markup(reply_markup=None)
+    await call.message.delete()
     await call.message.answer("❌ Ro‘yxatdan o‘tish bekor qilindi.", reply_markup=main_keyboard())
     await state.finish()
